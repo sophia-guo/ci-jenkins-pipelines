9f8015703da8e33bfa14ac523618da333a618183
Sophia Guo <sophia.gwf@gmail.com>
2025-09-30 11:37:45 -0400

    Internal IBM
    Signed-off-by: Sophia Guo <sophia.gwf@gmail.com>

--- pipelines/jobs/configurations/jdk23u_evaluation.groovy
+++
@@ -1,9 +0,0 @@
-targetConfigurations = []
-
-// scmReferences to use for weekly evaluation release build
-weekly_evaluation_scmReferences = [
-        'hotspot'        : '',
-        'temurin'        : ''
-]
-
-return this
--- pipelines/jobs/configurations/jdk26_pipeline_config.groovy
+++ pipelines/jobs/configurations/jdk26_pipeline_config.groovy
@@ -1,3 +1,4 @@
+
 class Config26 {
 
     final Map<String, Map<String, ?>> buildConfigurations = [
